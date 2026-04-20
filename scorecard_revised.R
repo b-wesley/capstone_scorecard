fed47cbbc6b4a5d30a33434260cb0e93e38b66f1
b-wesley <brantw48@gmail.com>
2026-04-20 16:12:45 -0400

    scorecard methodology update

--- scorecard_revised.R
+++ scorecard_revised.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(dplyr)
 
-scores <- read.csv("scorecards2.csv")
+scores <- read.csv("final_score_input.csv")
 
 scores_out <- scores %>%
-  mutate(final_score = (Heat * heat_weight) + 
+  mutate(raw_score = (Heat * heat_weight) + 
                        (Coastal_Flooding * coastal_flood_weight) +
                        (Cold_Wave * cold_wave_weight) +
                        (Wildfire * wildfire_weight) + 
@@ -12,6 +12,11 @@ scores_out <- scores %>%
                        (Drought * drought_weight) + 
                        (Hurricane * hurricane_weight) +
                        (Extreme_Wind * wind_weight) +
-                       (Winter_Storm * winter_storm_weight))  %>%
-  select(State, final_score) %>%
+                       (Winter_Storm * winter_storm_weight),
+          denominator = 4 * 
+                       (heat_weight + coastal_flood_weight + cold_wave_weight + 
+                       wildfire_weight + inland_flood_risk + drought_weight + 
+                       hurricane_weight + wind_weight + winter_storm_weight),
+          normalized_score = raw_score / denominator)  %>%
+  select(State, raw_score, normalized_score) %>%
   write.csv("revised_scorecard_output.csv")
